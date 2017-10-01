6cbcc04dbe784d702673dce238980b8d483db93c
Pham Hong Long <longph1989@gmail.com>
2017-10-01 21:31:14 +0800

    change name

--- src/main/costar/CoStar.java
+++ src/main/costar/CoStar.java
@@ -1,28 +1,28 @@
-package constar;
+package costar;
 
 import gov.nasa.jpf.Config;
 import gov.nasa.jpf.jdart.JDart;
 import gov.nasa.jpf.jdart.config.ConcolicConfig;
 
-public class ConStar extends JDart {
+public class CoStar extends JDart {
 
-	public ConStar(Config conf) {
+	public CoStar(Config conf) {
 		super(conf);
 	}
 
-	public ConStar(Config conf, boolean init) {
+	public CoStar(Config conf, boolean init) {
 		super(conf, new ConcolicConfig(conf), init);
 	}
 
-	public ConStar(Config conf, ConcolicConfig cc) {
+	public CoStar(Config conf, ConcolicConfig cc) {
 		super(conf, cc, false);
 	}
 
-	public ConStar(ConcolicConfig cc) {
+	public CoStar(ConcolicConfig cc) {
 		super(cc);
 	}
 
-	public ConStar(Config conf, ConcolicConfig cc, boolean init) {
+	public CoStar(Config conf, ConcolicConfig cc, boolean init) {
 		super(conf, cc, init);
 	}
 
