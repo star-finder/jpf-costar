7166d3d273cbe8ef28f2a5520d0fc0a54f28502e
Pham Hong Long <longph1989@gmail.com>
2017-10-01 22:00:19 +0800

    rename

---
+++ src/examples/features/sll/SllTest.java
@@ -0,0 +1,27 @@
+package features.sll;
+
+public class SllTest {
+	
+	public void travel(Node root) {
+		if (root == null)
+			return;
+		else if (root.next == null)
+			return;
+		else
+			return;
+	}
+
+}
+
+class Node {
+	
+	int elem;
+	
+	Node next;
+	
+	public Node(int elem, Node next) {
+		this.elem = elem;
+		this.next = next;
+	}
+	
+}
