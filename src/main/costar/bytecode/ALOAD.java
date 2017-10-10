0912cf665ab593cf6d3f165d97d0c862e3dc490a
Pham Hong Long <longph1989@gmail.com>
2017-10-11 00:17:14 +0800

    symbolic value for ref

--- src/main/costar/bytecode/ALOAD.java
+++ src/main/costar/bytecode/ALOAD.java
@@ -1,9 +1,32 @@
 package costar.bytecode;
 
+import gov.nasa.jpf.Config;
+import gov.nasa.jpf.vm.ElementInfo;
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.StackFrame;
+import gov.nasa.jpf.vm.ThreadInfo;
+
 public class ALOAD extends gov.nasa.jpf.jvm.bytecode.ALOAD {
 
 	public ALOAD(int index) {
 		super(index);
+	}
+	
+	@Override
+	public Instruction execute(ThreadInfo ti) {
+		Config conf = ti.getVM().getConfig();
+		StackFrame sf = ti.getModifiableTopFrame();
+		
+		int objRef = sf.peek();
+		ElementInfo ei = ti.getElementInfo(objRef);
+		if (ei != null) {
+			Object sym_v = ei.getObjectAttr();
+			
+			if (sym_v != null)
+				System.out.println(sym_v);
+		}
+		
+		return super.execute(ti);
 	}
 
 }
