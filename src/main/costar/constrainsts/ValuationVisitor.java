0be1d79fc1c241c3703d8bf7d180a6718b0435e5
Pham Hong Long <longph1989@gmail.com>
2017-10-16 15:48:22 +0800

    pure valuation visitor

--- src/main/costar/constrainsts/ValuationVisitor.java
+++ src/main/costar/constrainsts/ValuationVisitor.java
@@ -1,8 +1,15 @@
 package costar.constrainsts;
 
+import java.util.List;
+
 import gov.nasa.jpf.constraints.api.Valuation;
 import gov.nasa.jpf.constraints.api.ValuationEntry;
 import gov.nasa.jpf.constraints.types.BuiltinTypes;
+import gov.nasa.jpf.constraints.types.Type;
+import starlib.formula.Variable;
+import starlib.formula.expression.Comparator;
+import starlib.formula.expression.Expression;
+import starlib.formula.expression.VariableExpression;
 import starlib.formula.pure.ComparisonTerm;
 import starlib.jpf.testsuites.PathFinderVisitor;
 
@@ -17,9 +24,55 @@ public class ValuationVisitor extends PathFinderVisitor {
 
 	@Override
 	public void visit(ComparisonTerm term) {
-		ValuationEntry e = new ValuationEntry(new gov.nasa.jpf.constraints.api.Variable(BuiltinTypes.SINT32, "i"),
-				new Integer(-5));
-		val.addEntry(e);
+		Comparator comp = term.getComparator();
+		Expression exp1 = term.getExp1();
+		Expression exp2 = term.getExp2();
+		
+		List<Variable> vars1 = exp1.getVars();
+		List<Variable> vars2 = exp2.getVars();
+		
+		Variable var = null;
+		Type type = null;
+		Object value = null;
+		String name = null, typeStr = null, valueStr = null;
+		
+		if (comp == Comparator.EQ && exp1 instanceof VariableExpression && 
+				!initVars.containsAll(vars1) && (vars2.isEmpty() || initVars.containsAll(vars2))) {
+			var = ((VariableExpression) exp1).getVar();
+			valueStr = exp2.toString();
+		}
+		
+		if (comp == Comparator.EQ && exp2 instanceof VariableExpression && 
+				!initVars.containsAll(vars2) && (vars1.isEmpty() || initVars.containsAll(vars1))) {
+			var = ((VariableExpression) exp1).getVar();
+			valueStr = exp2.toString();
+		}
+		
+		if (var != null) {
+			initVars.add(var);
+			name = var.getName();
+			typeStr = var.getType();
+		}
+		
+		if (typeStr != null) {
+			if (typeStr.equals("boolean")) {
+				if (valueStr.equals("0"))
+					valueStr = "false";
+				else
+					valueStr = "true";
+				
+				type = BuiltinTypes.BOOL;
+				value = new Boolean(valueStr);
+			} else if (typeStr.equals("int")) {
+				type = BuiltinTypes.SINT32;
+				value = new Integer(valueStr);
+			}
+		}
+		
+		if (type != null && name != null && value != null) {
+			ValuationEntry e = new ValuationEntry(new gov.nasa.jpf.constraints.api.Variable(type, name), value);
+			val.addEntry(e);
+		}
 	}
 
 }
