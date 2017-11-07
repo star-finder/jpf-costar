092c278825c926fe5adbf07b33a50c9249e3c73c
Quoc-Sang Phan <dark2bright@gmail.com>
2017-11-06 16:55:59 -0800

    simple printing

--- src/main/costar/testgeneration/TestGenerator.java
+++ src/main/costar/testgeneration/TestGenerator.java
@@ -7,7 +7,13 @@ import gov.nasa.jpf.constraints.api.Valuation;
 public class TestGenerator {
 	
 	public static void generateTestSuites(ArrayList<Valuation> allValuations) {
-		
+		allValuations.forEach(valuation->{
+			valuation.forEach(ve->{
+				gov.nasa.jpf.constraints.api.Variable var = ve.getVariable();
+				System.out.println("\n\n================ Test case");
+				System.out.println(var.getType() + " " + var.getName() + " = " + ve.getValue().toString() );
+			});
+		});
 	}
 
 }
