46052def462a5da8fd3ac379cd63a09f7827485c
Pham Hong Long <longph1989@gmail.com>
2017-12-05 22:15:22 +0800

    dll bug with integer

---
+++ src/examples/star/doublelinkedlist/Driver.java
@@ -0,0 +1,77 @@
+package star.doublelinkedlist;
+
+import doublelinkedlist.DoubleLinkedList;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		if (args.length < 1)
+			return;
+		DoubleLinkedList list = new DoubleLinkedList();
+		switch (args[0]) {
+		case "addFirst":
+			list.addFirst(null);
+			break;
+		case "addI":
+			list.addI(0,null);
+			break;
+		case "addLast":
+			list.addLast(null);
+			break;
+		case "add":
+			list.add(null);
+			break;	
+		case "clear":
+			list.clear();
+			break;
+		case "clone":
+			list.clone();
+			break;
+		case "contains":
+			list.contains(null);
+			break;
+		case "getFirst":
+			list.getFirst();
+			break;
+		case "getLast":
+			list.getLast();
+			break;
+		case "get":
+			list.get(0);
+			break;
+		case "indexOf":
+			list.indexOf(null);
+			break;
+		case "inList":
+			list.inList(null);
+			break;
+		case "lastIndexOf":
+			list.lastIndexOf(null);
+			break;
+		case "removeFirst":
+			list.removeFirst();
+			break;
+		case "removeI":
+			list.removeI(0);
+			break;
+		case "removeLast":
+			list.removeLast();
+			break;
+		case "remove":
+			list.remove(null);
+			break;
+		case "set":
+			list.set(0,null);
+			break;
+		case "size":
+			list.size();
+			break;
+		case "toArray":
+			list.toArray();
+			break;
+		default:
+			// do nothing
+		}
+	}
+
+}
